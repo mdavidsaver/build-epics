36e8be84d62fd82dcec3f092d6a9954219338914
Michael Davidsaver <mdavidsaver@gmail.com>
2018-03-02 14:48:44 -0800

    remove unnecessary prefix

--- build-all-except-ad.sh
+++ build-all-except-ad.sh
@@ -4,8 +4,8 @@ set -e -x
 #
 # This script calls the other subscripts in the correct order.
 
-./build-epics/build-epics.sh
-./build-epics/build-exp-ctl-support.sh
-./build-epics/build-exp-ctl-support-sscan7.sh
-./build-epics/build-exp-ctl-iocs.sh
+./build-epics.sh
+./build-exp-ctl-support.sh
+./build-exp-ctl-support-sscan7.sh
+./build-exp-ctl-iocs.sh
 
--- build-all.sh
+++ build-all.sh
@@ -4,10 +4,10 @@ set -e -x
 #
 # This script calls the other subscripts in the correct order.
 
-./build-epics/build-epics.sh
-./build-epics/build-exp-ctl-support.sh
-./build-epics/build-exp-ctl-support-sscan7.sh
-./build-epics/build-exp-ctl-support-ad7.sh
-./build-epics/build-exp-ctl-iocs.sh
-./build-epics/build-exp-ctl-ioc-ad.sh
+./build-epics.sh
+./build-exp-ctl-support.sh
+./build-exp-ctl-support-sscan7.sh
+./build-exp-ctl-support-ad7.sh
+./build-exp-ctl-iocs.sh
+./build-exp-ctl-ioc-ad.sh
 
