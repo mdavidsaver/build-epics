9e8d9ba7aa6cc8d9fae2ccc64bdbe5fce1471f72
Michael Davidsaver <mdavidsaver@gmail.com>
2022-03-23 22:38:01 -0700

    add deb

--- docker-build.sh
+++ docker-build.sh
@@ -4,9 +4,7 @@ set -e -x
 cat /etc/os-release
 . /etc/os-release
 
-PKG=dnf
-
-case "$ID/$VERSION" in
+case "$ID/$VERSION_ID" in
 centos/7*)
     yum -y install epel-release
     PKG=yum
@@ -22,18 +20,37 @@ centos/8*)
 
     dnf -y install python2
     alternatives --set python /usr/bin/python2
+    PKG=dnf
+    ;;
+debian/*)
+    PKG=apt-get
     ;;
 *)
-    echo "??? $ID/$VERSION ???"
+    echo "??? $ID/$VERSION_ID ???"
     exit 1
     ;;
 esac
 
-$PKG -y install perl gcc-c++ make git \
-    glibc-devel make readline-devel ncurses-devel perl-devel \
-    pkg-config pcre-devel re2c GraphicsMagick-c++-devel libjpeg-turbo-devel \
-    libtiff-devel libXext-devel hdf5-devel libaec-devel netcdf-devel \
-    libxml2-devel tar autoconf automake libtool python3 rpcgen libtirpc-devel
+case "$PKG" in
+yum|dnf)
+    $PKG -y install perl gcc-c++ make git \
+        glibc-devel make readline-devel ncurses-devel perl-devel \
+        pkg-config pcre-devel re2c GraphicsMagick-c++-devel libjpeg-turbo-devel \
+        libtiff-devel libXext-devel hdf5-devel libaec-devel netcdf-devel \
+        libxml2-devel tar autoconf automake libtool python3 rpcgen libtirpc-devel
+    ;;
+apt-get)
+    $PKG update
+    $PKG -y install git build-essential libreadline6-dev libncurses5-dev perl libpcre3-dev re2c python3-dev python3-nose \
+        python3-numpy libgraphicsmagick++-dev libhdf5-dev libjpeg-dev libnetcdf-dev libtiff-dev libz-dev \
+        python-is-python3
+
+    ;;
+*)
+    echo "??? $PKG ???"
+    exit 1
+    ;;
+esac
 
 ./build-epics.sh -j2 </dev/null
 
