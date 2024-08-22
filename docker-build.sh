58ff94b4cd2589270cae4329b7ef70580649f4e4
Michael Davidsaver <mdavidsaver@gmail.com>
2024-08-21 17:28:58 -0700

    GHA fixup

--- docker-build.sh
+++ docker-build.sh
@@ -13,6 +13,9 @@ rh_install() {
 
 case "$ID/$VERSION_ID" in
 centos/7*)
+    sed -i s/mirror.centos.org/vault.centos.org/g /etc/yum.repos.d/*.repo
+    sed -i s/^#.*baseurl=http/baseurl=http/g /etc/yum.repos.d/*.repo
+    sed -i s/^mirrorlist=http/#mirrorlist=http/g /etc/yum.repos.d/*.repo
     yum -y install epel-release
     rh_install yum
     ;;
